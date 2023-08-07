16f6d23ab598cec285b76d24c0bc6839f33d5f14
pappasa <pappasa@unisystems.gr>
2023-08-07 14:05:50 +0300

    Add files via upload

---
+++ Models/UserMasterRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace TokenAuthenticationInWebAPI.Models
+{
+    public class UserMasterRepository : IDisposable
+    {
+        // SECURITY_DBEntities it is your context class
+        SECURITY_DBEntities context = new SECURITY_DBEntities();
+
+        //This method is used to check and validate the user credentials
+        public UserMaster ValidateUser(string username, string password)
+        {
+            return context.UserMasters.FirstOrDefault(user =>
+            user.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+            && user.UserPassword == password);
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
